672ebac92d925d35c4ac89b02163296fe05dae75
Edward Boado <eboado8862@conestogac.on.ca>
2022-09-17 17:40:39 -0400

    remove local port variables to debug heroku

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const cors = require ('cors');
 const morgan = require('morgan');
-require('dotenv/config');
+//require('dotenv/config');
 
 // Allow http requests from any origin
 app.use(cors());    
